e522e25af41b2668fa8b8d085ba4a5dd2f91bec1
Javi <kientienemibarraespaciadora@gmail.com>
2013-08-09 15:12:49 +0200

    Initial Commit

---
+++ BlocksExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BlocksExample
+//
+//  Created by Javier Delgado on 09/08/13.
+//  Copyright (c) 2013 Javier Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlocksExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BlocksExample
+//
+//  Created by Javier Delgado on 09/08/13.
+//  Copyright (c) 2013 Javier Delgado. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
